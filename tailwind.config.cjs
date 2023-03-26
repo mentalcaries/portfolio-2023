647befaa03327609437cf1a1b11d6c8477e5cf7b
Devin Jaggernauth <neexis@gmail.com>
2023-03-26 14:12:59 -0400

    add profile photo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'Arial', 'ui-sans-serif'],
+			serif: ['Quattrocento', 'Georgia'],
+			body: ['Hind', 'ui-sans-serif']
+    },
+  },
+  plugins: [],
+};
